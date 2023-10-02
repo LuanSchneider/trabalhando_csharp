4370dac8745460b0931d2234c09512e96fc45624
LuanSchneider <143182587+LuanSchneider@users.noreply.github.com>
2023-10-02 09:44:35 -0300

    Adicionar arquivos de projeto.

---
+++ C#case/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace consoleApp
+{
+    class program
+    {
+        static Main(string[] args)
+        {
+            int numero = 10;
+            int numero_2 = 5;
+            Console.WriteLine("quem é "+numero+"+"+numero_2+ "?");
+            int resposta=Convert.ToInt32(Console.ReadLine());
+            if (resposta == 15)
+            {
+                Console.WriteLine("parabens!! ");
+            }
+            else
+                Console.WriteLine("tente de novo");
+
+        }
+    }
+}
